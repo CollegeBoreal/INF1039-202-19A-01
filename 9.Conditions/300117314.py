0240291c65e0b3ddbf8e07f68c5e47fa221a0513
morti747 <54910700+morti747@users.noreply.github.com>
2019-12-10 23:16:13 -0500

    morti
    condition

---
+++ 9.Conditions/300117314.py
@@ -0,0 +1,17 @@
+"""
+Created on Tue Dec 10 21:54:01 2019
+
+@author: morti
+"""
+ 
+
+age = int(input("entrer votre age: "))
+
+
+if age >= 18: 
+
+   print("Vous êtes majeur")
+
+else:
+    print("Vous êtes mineur")
+    
